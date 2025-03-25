10e5f568f76e8b1bc686a50b8447d98e1bd2bedc
Mikhail Ikonnikov <mishaikon@gmail.com>
2025-03-26 00:10:47 +0300

    - chg db from pg to sqlite in config.cjs

--- config/config.cjs
+++ config/config.cjs
@@ -16,23 +16,23 @@ module.exports = {
     host: process.env.DATABASE_HOST,
   },
 
-  test: {
-    dialect: 'postgres',
-    database: process.env.DATABASE_NAME || 'postgres',
-    username: process.env.DATABASE_USERNAME || 'postgres',
-    password: process.env.DATABASE_PASSWORD || 'password',
-    host: process.env.DATABASE_HOST || 'db',
-    port: process.env.DATABASE_PORT || 5432,
+  // test: {
+  //   dialect: 'postgres',
+  //   database: process.env.DATABASE_NAME || 'postgres',
+  //   username: process.env.DATABASE_USERNAME || 'postgres',
+  //   password: process.env.DATABASE_PASSWORD || 'password',
+  //   host: process.env.DATABASE_HOST || 'db',
+  //   port: process.env.DATABASE_PORT || 5432,
     // Опционально можно добавить:
     // migrations: {
     //   directory: './migrations',
     // },
-  },
+//  },
 
-  // test: {
-  //   dialect: 'sqlite',
-  //   storage: './database.test.sqlite',
-  // },
+  test: {
+     dialect: 'sqlite',
+     storage: './database.test.sqlite',
+  },
 
   // test: {
   //   dialect: 'postgres',
