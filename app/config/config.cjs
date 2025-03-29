709aecae182ea17bebfb97d7c699d6e5c30378e0
Mikhail Ikonnikov <mishaikon@gmail.com>
2025-03-29 23:08:45 +0300

    step 6 + add /app

---
+++ app/config/config.cjs
@@ -0,0 +1,47 @@
+require('dotenv').config();
+
+module.exports = {
+
+  development: {
+    dialect: 'sqlite',
+    storage: './database.sqlite',
+  },
+
+  production: {
+    dialect: 'postgres',
+    database: process.env.DATABASE_NAME,
+    username: process.env.DATABASE_USERNAME,
+    password: process.env.DATABASE_PASSWORD,
+    port: process.env.DATABASE_PORT,
+    host: process.env.DATABASE_HOST,
+  },
+
+  test: {
+    client: 'pg',
+    connection: {
+      host: process.env.DATABASE_HOST || 'db',
+      database: process.env.DATABASE_NAME || 'postgres',
+      user: process.env.DATABASE_USERNAME || 'postgres',
+      password: process.env.DATABASE_PASSWORD || 'password',
+    },
+    migrations: {
+      directory: './migrations',
+    },
+    seeds: {
+      directory: './seeds',
+    },
+  },
+
+  // test: {
+  //   dialect: 'sqlite',
+  //   storage: './database.test.sqlite',
+  // },
+  // test: {
+  //   dialect: 'postgres',
+  //   database: process.env.DATABASE_NAME,
+  //   username: process.env.DATABASE_USERNAME,
+  //   password: process.env.DATABASE_PASSWORD,
+  //   port: process.env.DATABASE_PORT,
+  //   host: process.env.DATABASE_HOST,
+  // },
+};
