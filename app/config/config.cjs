70d2c75317f335174f391507b9c8766337e6b029
Mikhail Ikonnikov <mishaikon@gmail.com>
2025-03-29 21:04:15 +0300

    rollback to working push.yml

--- app/config/config.cjs
+++ app/config/config.cjs
@@ -1,12 +1,10 @@
 require('dotenv').config();
 
 module.exports = {
-
   development: {
     dialect: 'sqlite',
     storage: './database.sqlite',
   },
-
   production: {
     dialect: 'postgres',
     database: process.env.DATABASE_NAME,
@@ -15,30 +13,10 @@ module.exports = {
     port: process.env.DATABASE_PORT,
     host: process.env.DATABASE_HOST,
   },
-
-  // test: {
-  //   dialect: 'sqlite',
-  //   storage: './database.test.sqlite',
-  // },
-
   test: {
-    client: 'postgresql',
-    connection: {
-      host: process.env.DATABASE_HOST,
-      database: process.env.DATABASE_NAME,
-      user: process.env.DATABASE_USERNAME,
-      password: process.env.DATABASE_PASSWORD,
-      dialect: 'postgres' // Явно указываем диалект
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  }
-
+    dialect: 'sqlite',
+    storage: './database.test.sqlite',
+  },
   // test: {
   //   dialect: 'postgres',
   //   database: process.env.DATABASE_NAME,
